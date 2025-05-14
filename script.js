61b70b38ac21f0024f494605c9f2e3fc0714156d
Te2t007 <pathomphon1967@gmail.com>
2025-05-14 20:38:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showPage(pageId) {
+  // ซ่อน header และหน้าอื่นๆ
+  document.querySelector('header').style.display = 'none';
+  document.querySelectorAll('.page').forEach(page => page.classList.remove('visible'));
+  
+  // แสดงหน้าที่เลือก
+  document.getElementById(pageId).classList.add('visible');
+}
+
+function showHome() {
+  // แสดง header และซ่อนหน้าอื่นๆ
+  document.querySelector('header').style.display = 'block';
+  document.querySelectorAll('.page').forEach(page => page.classList.remove('visible'));
+}
